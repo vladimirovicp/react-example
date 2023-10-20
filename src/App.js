0d3d0284d0d01ee7a9c7c1293e2187e4bc8f158a
vladimirovicp <vladimirovicp70@gmail.com>
2023-10-20 11:34:00 +0400

    start: task react

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Nav from "./components/Nav";
+
+import Home from "./page/Home";
+import Page1 from "./page/Page1";
+import Page2 from "./page/Page2";
+import Page3 from "./page/Page3";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <div className="App">
+            <Nav />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/page-1" element={<Page1 />} />
+              <Route path="/page-2" element={<Page2 />} />
+              <Route path="/page-3" element={<Page3 />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+function Nav() {
+    return (
+        <nav className="nav-links">
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/page-1'>Page 1</Link></li>
+                <li><Link to='/page-2'>Page 2</Link></li>
+                <li><Link to='/page-3'>Page 3</Link></li>
+            </ul>
+        </nav>
+    );
+}
+export default Nav;
