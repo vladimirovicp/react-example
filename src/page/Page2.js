7a479f27a237d6d743fff698feacbd46dc5fb4e0
vladimirovicp <vladimirovicp70@gmail.com>
2023-10-24 12:52:19 +0400

    save pc

--- src/page/Page2.js
+++ src/page/Page2.js
@@ -1,8 +1,31 @@
 function Page2() {
 
+    const handleSaveToPC = (jsonData,filename) => {
+        const fileData = JSON.stringify(jsonData);
+        const blob = new Blob([fileData], {type: "text/plain"});
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.download = `${filename}.json`;
+        link.href = url;
+        link.click();
+      }
+
+      const fileJson = [{
+        name: 'test',
+        name2: 'test2'
+    }];
+
+      const handleSubmit = event => {
+        handleSaveToPC(fileJson,'test');
+      }
+
+
     return (
         <div className="App">
-            <h1>Page 2</h1>
+            <h1>Page 2 Save data json to PC</h1>
+            <form onSubmit={handleSubmit}>
+            <button type="submit">Save yo PC</button>
+            </form>
         </div>
     );
 }
